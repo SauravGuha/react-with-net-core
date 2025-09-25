731b1cad33d7b91006c4f424b5dfa8a324040e88
Saurav Guha <saurav.g@appseconnect.com>
2025-09-25 15:35:33 +0530

    final commit

--- backend/Domain/Repositories/BaseRepositories/ICommandRepository.cs
+++ backend/Domain/Repositories/BaseRepositories/ICommandRepository.cs
@@ -4,11 +4,11 @@ namespace Domain.Repositories
 {
     public interface ICommandRepository<T>
     {
-        Task<T> CreateAsync(T entity);
+        Task<T> CreateAsync(T entity, CancellationToken token);
 
-        Task<T> UpdateAsync(T entity);
+        Task<T> UpdateAsync(T entity, CancellationToken token);
 
-        Task<int> DeleteAsync(T Entity);
+        Task<int> DeleteAsync(T Entity, CancellationToken token);
 
     }
 }
--- backend/Persistence/Repository/ActivityRepository/ActivityCommandRepository.cs
+++ backend/Persistence/Repository/ActivityRepository/ActivityCommandRepository.cs
@@ -14,28 +14,31 @@ namespace Persistence.Repository.ActivityRepository
 
         }
 
-        public async Task<Activity> CreateAsync(Activity entity)
+        public async Task<Activity> CreateAsync(Activity entity, CancellationToken token)
         {
             var activity = activityDbContext.Activities.FirstOrDefault(e => e.Title.ToLower() == entity.Title.ToLower()
             && e.EventDate == entity.EventDate && e.Category.ToLower() == entity.Category.ToLower());
             if (activity == null)
             {
                 activityDbContext.Activities.Add(entity);
-                await activityDbContext.SaveChangesAsync();
+                await activityDbContext.SaveChangesAsync(token);
                 return entity;
             }
             else
                 throw new InvalidOperationException("Cannot create same event on the same date");
         }
 
-        public Task<int> DeleteAsync(Activity Entity)
+        public async Task<int> DeleteAsync(Activity entity, CancellationToken token)
         {
-            throw new NotImplementedException();
+            activityDbContext.Activities.Remove(entity);
+            return await activityDbContext.SaveChangesAsync(token);
         }
 
-        public Task<Activity> UpdateAsync(Activity entity)
+        public async Task<Activity> UpdateAsync(Activity entity, CancellationToken token)
         {
-            throw new NotImplementedException();
+            activityDbContext.Activities.Update(entity);
+            await activityDbContext.SaveChangesAsync(token);
+            return entity;
         }
     }
 }
