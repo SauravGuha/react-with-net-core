01bb27403b049da81da601bb05a922adf2886025
saurav guha <sauravguha0112@gmail.com>
2025-10-22 22:54:59 +0530

    profile content completed

--- frontend/client/src/features/profile/ProfileContent.tsx
+++ frontend/client/src/features/profile/ProfileContent.tsx
@@ -1,7 +1,37 @@
+import { Box, Paper, Tab, Tabs } from "@mui/material";
+import { useState, type SyntheticEvent } from "react";
 
 
 export default function ProfileContent() {
+    const [value, setValue] = useState(0);
+    const tabContent = [
+        { label: 'About', content: <div>About</div> },
+        { label: 'Photos', content: <div>Photos</div> },
+        { label: 'Events', content: <div>Events</div> },
+        { label: 'Followers', content: <div>Followers</div> },
+        { label: 'Following', content: <div>Following</div> }
+    ];
+
+    const handleChange = (event: SyntheticEvent, newValue: number) => {
+        setValue(newValue);
+    };
     return (
-        <div>ProfileContent</div>
+        <Box component={Paper} sx={{ display: 'flex', alignItems: 'flex-start', mt: 2, p: 3, height: 500 }} elevation={3}>
+            <Tabs
+                orientation="vertical"
+                variant="scrollable"
+                value={value}
+                onChange={handleChange}
+                aria-label="Vertical tabs example"
+                sx={{ borderRight: 1, height: 450, minWidth: 200 }}
+            >
+                {tabContent.map((item, index) =>
+                    <Tab key={index} label={item.label} sx={{ mr: 3 }} />
+                )}
+            </Tabs>
+            <Box sx={{ flexGrow: 1, p: 3 }}>
+                {tabContent[value].content}
+            </Box>
+        </Box>
     )
 }
--- frontend/client/src/features/profile/ProfilePage.tsx
+++ frontend/client/src/features/profile/ProfilePage.tsx
@@ -1,15 +1,15 @@
 import { Grid } from "@mui/material";
 import ProfileHeader from "./ProfileHeader";
 import ProfileContent from "./ProfileContent";
-import { useParams } from "react-router-dom";
-import useAccountReactQuery from "../../hooks/useAccountReactQuery";
-import NotFound from "../../app/component/NotFound";
+// import { useParams } from "react-router-dom";
+// import useAccountReactQuery from "../../hooks/useAccountReactQuery";
+
 
 
 export default function ProfilePage() {
-    const { userData } = useAccountReactQuery();
-    const { id } = useParams();
-    if (userData!.id != id) return <NotFound />
+    // const { userData } = useAccountReactQuery();
+    // const { id } = useParams();
+    // if (userData!.id != id) return <NotFound />
     return (
         <Grid container>
             <Grid size={12}>
