28a801109ddaba788d5cc10e011fcde6879d9e49
saurav guha <sauravguha0112@gmail.com>
2025-10-25 14:02:56 +0530

    Commentrequest and handler added

--- backend/Application/Profiles/CommentProfile.cs
+++ backend/Application/Profiles/CommentProfile.cs
@@ -11,6 +11,9 @@ namespace Application.Profiles
         public CommentProfile()
         {
             CreateMap<CommentCommandViewModel, Domain.Models.Comment>();
+            CreateMap<Domain.Models.Comment, CommentViewModel>()
+            .ForMember(cvm => cvm.DisplayName, mo => mo.MapFrom(c => c.User.DisplayName))
+            .ForMember(cvm => cvm.ImageUrl, mo => mo.MapFrom(c => c.User.ImageUrl));
         }
     }
 }
